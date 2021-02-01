ee2a3a80adddf7ac08c4807fa600cfbd70386db1
Rubin Bhandari <roobin.bhandari@gmail.com>
2021-02-01 10:35:59 +0545

    fix: pagination utils

---
+++ src/paginate.utils.ts
@@ -0,0 +1,81 @@
+import { classToPlain } from 'class-transformer';
+
+
+
+/**
+ *
+ *
+ * @interface Ipagination
+ */
+
+interface Ipagination {
+	total_pages: number;
+	total_items: number;
+	next: string;
+	previous: string;
+	current_page: number;
+	items: any;
+}
+
+
+/**
+ *
+ *
+ * @export
+ * @param {number} page
+ * @param {number} totalPages
+ * @returns {string}
+ */
+export function hasNext(page: number, totalPages: number): string {
+	if (page === totalPages) {
+		return '';
+	} else {
+		return `${page + 1}`;
+	}
+}
+
+
+/**
+ *
+ *
+ * @export
+ * @param {number} page
+ * @returns {string}
+ */
+export function hasPrevious(page: number): string {
+	if (page <= 1) {
+		return '';
+	} else {
+		return `${page - 1}`;
+	}
+}
+
+
+
+/**
+ *
+ *
+ * @export
+ * @param {number} pages
+ * @param {number} page
+ * @param {number} total
+ * @param {string} host
+ * @param {Array<any>} result
+ * @returns {Ipagination}
+ */
+export function paginate(
+	pages: number,
+	page: number,
+	total: number,
+	host: string,
+	result: Array<any>,
+): Ipagination {
+	return {
+		total_pages: pages,
+		total_items: total,
+		next: hasNext(page, pages),
+		previous: hasPrevious(page),
+		current_page: page,
+		items: classToPlain(result),
+	};
+}
