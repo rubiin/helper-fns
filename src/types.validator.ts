98d834ec82a77b151d6b8da362868802dd9036aa
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-08-07 19:59:12 +0545

    fix: types validator

--- src/types.validator.ts
+++ src/types.validator.ts
@@ -1,6 +1,6 @@
 import { toString } from "./base";
 
-export const isDef = <T = any>(val?: T): val is T => typeof val !== "undefined";
+export const isDefined = <T = any>(val?: T): val is T => typeof val !== "undefined";
 export const isBoolean = (val: any): val is boolean => typeof val === "boolean";
 export const isFunction = <T extends Function>(val: any): val is T =>
   typeof val === "function";
@@ -9,3 +9,7 @@ export const isString = (val: unknown): val is string =>
   typeof val === "string";
 export const isObject = (val: any): val is object =>
   toString(val) === "[object Object]";
+
+export const isNull = (value: unknown): value is null => value === null;
+
+export const isArray = <T>(value: unknown): value is T[] => Array.isArray(value)
