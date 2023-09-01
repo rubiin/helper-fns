44c0e1b1c7fd3141d215272eeeadc0f3a3704e94
Rubin Bhandari <roobin.bhandari@gmail.com>
2023-09-01 22:38:49 +0545

    fix: add isPrimitive and isNonPrimitive

--- src/types.validator.ts
+++ src/types.validator.ts
@@ -1,11 +1,8 @@
 import { Buffer } from "node:buffer";
-import type { Class, FunctionType } from "./types";
+import type { Class, FunctionType, Primitive } from "./types";
 import { toString } from "./base";
 import { isDate, isEmpty, isSameDate } from "./function.utils";
 
-export function isDefined<T = any>(value?: T): value is T {
-  return value !== undefined;
-}
 export const isBoolean = (value: any): value is boolean =>
   typeof value === "boolean";
 export function isFunction<T extends FunctionType>(value: any): value is T {
@@ -20,6 +17,10 @@ export function isObject(value: any): value is object {
   return toString(value) === "[object Object]";
 }
 
+export const isUndefined = (value: unknown): value is undefined => {
+  return value === undefined;
+};
+
 export const isNull = (value: unknown): value is null => value === null;
 
 export function isArray<T>(value: unknown): value is T[] {
@@ -27,6 +28,10 @@ export function isArray<T>(value: unknown): value is T[] {
 }
 export function isBigint(value: unknown): value is bigint {
   return typeof value === "bigint";
+}
+
+export function isSymbol(value: unknown): value is symbol {
+  return typeof value === "symbol";
 }
 
 export function isBuffer(value: unknown): value is Buffer {
@@ -40,18 +45,29 @@ export function isRegExp(value: any): value is RegExp {
   return toString(value) === "[object RegExp]";
 }
 
+export function isPrimitive(value?: unknown): value is Primitive {
+  return [isNull, isUndefined, isString, isNumber, isBoolean, isSymbol, isBigint].some(function_ => function_(value));
+}
+
+export function isNonPrimitive(x?: unknown): x is object {
+  return !isPrimitive(x);
+}
+
 export const is = {
   isArray,
   isBoolean,
-  isDefined,
   isFunction,
   isNull,
   isNumber,
+  isUndefined,
   isObject,
+  isSymbol,
   isString,
   isBigint,
   isBuffer,
   isClass,
+  isPrimitive,
+  isNonPrimitive,
   isDate,
   isEmpty,
   isSameDate,
--- test/types.validators.test.ts
+++ test/types.validators.test.ts
@@ -3,109 +3,167 @@ import {
   isArray,
   isBigint,
   isBoolean,
-  isDefined,
+  isUndefined,
   isFunction,
   isNull,
   isNumber,
   isObject,
   isString,
-} from "../src";
+  isSymbol,
+  isPrimitive,
+  isNonPrimitive
+} from "../src"
 
 describe("isDefined", () => {
-  test("should return true if value is defined", () => {
-    expect(isDefined(1)).toBe(true);
-    expect(isDefined("abc")).toBe(true);
-    expect(isDefined()).toBe(false);
-    expect(isDefined()).toBe(false);
-  });
-});
+  it("should return false for other values", () => {
+    expect(isUndefined(null)).toBe(false)
+    expect(isUndefined(0)).toBe(false)
+    expect(isUndefined("")).toBe(false)
+    expect(isUndefined(false)).toBe(false)
+    expect(isUndefined({})).toBe(false)
+    // Add more test cases for different types/values that are not undefined
+  })
+})
 
 describe("isBoolean", () => {
   test("should return true if value is a boolean", () => {
-    expect(isBoolean(true)).toBe(true);
-    expect(isBoolean(false)).toBe(true);
-    expect(isBoolean(0)).toBe(false);
-    expect(isBoolean("true")).toBe(false);
-  });
-});
+    expect(isBoolean(true)).toBe(true)
+    expect(isBoolean(false)).toBe(true)
+    expect(isBoolean(0)).toBe(false)
+    expect(isBoolean("true")).toBe(false)
+  })
+})
 
 describe("isFunction", () => {
   test("should return true if value is a function", () => {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    expect(isFunction(() => {})).toBe(true);
-    expect(isFunction({})).toBe(false);
-  });
-});
+    expect(isFunction(() => {})).toBe(true)
+    expect(isFunction({})).toBe(false)
+  })
+})
 
 describe("isNumber", () => {
   test("should return true if value is a number", () => {
-    expect(isNumber(1)).toBe(true);
-    expect(isNumber(0)).toBe(true);
-    expect(isNumber(-1)).toBe(true);
-    expect(isNumber("1")).toBe(false);
-    expect(isNumber(null)).toBe(false);
-    expect(isNumber({})).toBe(false);
-  });
-});
+    expect(isNumber(1)).toBe(true)
+    expect(isNumber(0)).toBe(true)
+    expect(isNumber(-1)).toBe(true)
+    expect(isNumber("1")).toBe(false)
+    expect(isNumber(null)).toBe(false)
+    expect(isNumber({})).toBe(false)
+  })
+})
 
 describe("isString", () => {
   test("should return true if value is a string", () => {
-    expect(isString("abc")).toBe(true);
-    expect(isString("")).toBe(true);
-    expect(isString(123)).toBe(false);
-    expect(isString(null)).toBe(false);
-    expect(isString({})).toBe(false);
-  });
-});
+    expect(isString("abc")).toBe(true)
+    expect(isString("")).toBe(true)
+    expect(isString(123)).toBe(false)
+    expect(isString(null)).toBe(false)
+    expect(isString({})).toBe(false)
+  })
+})
 
 describe("isObject", () => {
   test("should return true if value is an object", () => {
-    expect(isObject({})).toBe(true);
-    expect(isObject({ key: "value" })).toBe(true);
-    expect(isObject([])).toBe(false);
-    expect(isObject(null)).toBe(false);
-    expect(isObject("")).toBe(false);
-  });
-});
+    expect(isObject({})).toBe(true)
+    expect(isObject({ key: "value" })).toBe(true)
+    expect(isObject([])).toBe(false)
+    expect(isObject(null)).toBe(false)
+    expect(isObject("")).toBe(false)
+  })
+})
 
 describe("isNull", () => {
   test("should return true if value is null", () => {
-    expect(isNull(null)).toBe(true);
-    expect(isNull("null")).toBe(false);
-    expect(isNull({})).toBe(false);
-    expect(isNull(0)).toBe(false);
-  });
-});
+    expect(isNull(null)).toBe(true)
+    expect(isNull("null")).toBe(false)
+    expect(isNull({})).toBe(false)
+    expect(isNull(0)).toBe(false)
+  })
+})
+
+describe("isSymbol", () => {
+  it("should return true if value is a symbol", () => {
+    expect(isSymbol(Symbol("test"))).toBe(true)
+  })
+
+  it("should return false for other values", () => {
+    expect(isSymbol(null)).toBe(false)
+    expect(isSymbol(0)).toBe(false)
+    expect(isSymbol("")).toBe(false)
+    expect(isSymbol(false)).toBe(false)
+    expect(isSymbol({})).toBe(false)
+    // Add more test cases for different types/values that are not symbols
+  })
+})
 
 describe("isArray", () => {
   test("should return true if value is an array", () => {
-    expect(isArray([])).toBe(true);
-    expect(isArray([1, 2, 3])).toBe(true);
+    expect(isArray([])).toBe(true)
+    expect(isArray([1, 2, 3])).toBe(true)
     // eslint-disable-next-line @typescript-eslint/no-array-constructor
-    expect(isArray(new Array())).toBe(true); // Array constructor
-    expect(isArray({})).toBe(false);
-    expect(isArray(null)).toBe(false);
-    expect(isArray("")).toBe(false);
-    expect(isArray(123)).toBe(false);
-  });
+    expect(isArray(new Array())).toBe(true) // Array constructor
+    expect(isArray({})).toBe(false)
+    expect(isArray(null)).toBe(false)
+    expect(isArray("")).toBe(false)
+    expect(isArray(123)).toBe(false)
+  })
 
   test("should return true if value is an array of specific type", () => {
-    expect(isArray<number>([1, 2, 3])).toBe(true);
-    expect(isArray<string>(["a", "b", "c"])).toBe(true);
-    expect(isArray<boolean>([true, false])).toBe(true);
-    expect(isArray<number>("")).toBe(false);
-    expect(isArray<string>([])).toBe(true);
-  });
-});
+    expect(isArray<number>([1, 2, 3])).toBe(true)
+    expect(isArray<string>(["a", "b", "c"])).toBe(true)
+    expect(isArray<boolean>([true, false])).toBe(true)
+    expect(isArray<number>("")).toBe(false)
+    expect(isArray<string>([])).toBe(true)
+  })
+})
 
 describe("isBigint", () => {
   test("should return true if value is a bigint", () => {
-    expect(isBigint(BigInt(123))).toBe(true);
-    expect(isBigint(BigInt(0))).toBe(true);
-    expect(isBigint(BigInt(-123))).toBe(true);
-    expect(isBigint(123)).toBe(false);
-    expect(isBigint("123")).toBe(false);
-    expect(isBigint(null)).toBe(false);
-    expect(isBigint({})).toBe(false);
-  });
-});
+    expect(isBigint(BigInt(123))).toBe(true)
+    expect(isBigint(BigInt(0))).toBe(true)
+    expect(isBigint(BigInt(-123))).toBe(true)
+    expect(isBigint(123)).toBe(false)
+    expect(isBigint("123")).toBe(false)
+    expect(isBigint(null)).toBe(false)
+    expect(isBigint({})).toBe(false)
+  })
+}) // Import the module that contains the functions
+
+describe("isPrimitive", () => {
+  it("should return true for primitive values", () => {
+    expect(isPrimitive(null)).toBe(true)
+    expect(isPrimitive()).toBe(true)
+    expect(isPrimitive("hello")).toBe(true)
+    expect(isPrimitive(123)).toBe(true)
+    expect(isPrimitive(false)).toBe(true)
+    expect(isPrimitive(Symbol(""))).toBe(true)
+    expect(isPrimitive(BigInt(123))).toBe(true)
+  })
+
+  it("should return false for non-primitive values", () => {
+    expect(isPrimitive({})).toBe(false)
+    expect(isPrimitive([])).toBe(false)
+    expect(isPrimitive(new Date())).toBe(false)
+    // Add more test cases for non-primitive values
+  })
+})
+
+describe("isNonPrimitive", () => {
+  it("should return true for non-primitive values", () => {
+    expect(isNonPrimitive({})).toBe(true)
+    expect(isNonPrimitive([])).toBe(true)
+    expect(isNonPrimitive(new Date())).toBe(true)
+    // Add more test cases for non-primitive values
+  })
+
+  it("should return false for primitive values", () => {
+    expect(isNonPrimitive(null)).toBe(false)
+    expect(isNonPrimitive()).toBe(false)
+    expect(isNonPrimitive("hello")).toBe(false)
+    expect(isNonPrimitive(123)).toBe(false)
+    expect(isNonPrimitive(false)).toBe(false)
+    expect(isNonPrimitive(Symbol(""))).toBe(false)
+    expect(isNonPrimitive(BigInt(123))).toBe(false)
+  })
+})
